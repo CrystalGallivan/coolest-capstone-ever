edde674fce0250591074c3c9a593948e23a20894
Your Name <crystalgallivan@gmail.com>
2020-04-30 17:37:10 -0600

    Southwest Template Reformat

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,55 +10,13 @@
 
   export default {
     name: "APP",
-    computed: {
-      // menu() {
-      //   if (this.$store.router.currentRoute.path == "/menu") {
-      //     return false;
-      //   } else {
-      //     return true;
-      //   }
-      // },
-      owned() {
-        //FIXME Will need to be changed for new admins that have no sites yet
-        return this.$store.state.userSites.mySites ? this.$store.state.userSites.mySites.length > 0 : false
-      },
-      user() {
-        return this.$store.state.user
-      },
-      // site() {
-      //   return this.$store.state.site
-      // }
-    },
-    methods: {
-      logout() {
-        this.$store.dispatch('logout', this.creds)
-
-      },
-      openSiteSelect() {
-        this.$store.dispatch('changeSite')
-      },
-      // clearRecipe() {
-      //   this.$store.dispatch('clearRecipe')
-      // }
-      createActiveRecipe() {
-        this.$store.dispatch('createActiveRecipe')
-      }
-    },
-    // mounted() {
-    //   $("#menu-toggle").click(function (e) {
-    //     e.preventDefault();
-    //     $("#wrapper").toggleClass("toggled");
-    //     $("#page-content-wrapper").toggleClass("toggled")
-    //   });
-    //   this.$store.dispatch("authenticate")
-    // },
     components: {
       Login
     }
   }
 </script>
 
-<style>
+<style scoped>
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -67,18 +25,5 @@
     color: whitesmoke;
     user-select: none;
     margin-top: 5%;
-  }
-
-  #toggle-btn {
-    background-color: white;
-  }
-
-  #app-toggle-selection {
-    color: rgb(5, 38, 45);
-    font-weight: bold;
-  }
-
-  .btn-group-toggle {
-    margin: 1%;
   }
 </style>
