44605da0494c384dae31f758530f5f3fac9099d7
Your Name <crystalgallivan@gmail.com>
2020-05-28 13:20:39 -0600

    cleanup

--- client/src/store.js
+++ client/src/store.js
@@ -618,59 +618,7 @@ export default new Vuex.Store({
     },
 
     //#endregion 
-    //#region == Scheduling --
-    // scheduled({ commit, dispatch }) {
-    //   var currentDate = new Date();
-    //   let currentHour = currentDate.getHours();
-    //   let currentMinute = currentDate.getMinutes();
-    //   let time = currentHour + ':' + currentMinute
-    //   let scheduledStartTime = this.state.activeSign.beginningTime
-    //   let startTime = scheduledStartTime.split(new RegExp(':'))
-    //   let startHour = Number(startTime[0])
-    //   let startMinute = Number(startTime[1])
-    //   let scheduledEndTime = this.state.activeSign.endingTime
-    //   let endTime = scheduledEndTime.split(new RegExp(':'))
-    //   let endHour = Number(endTime[0])
-    //   let endMinute = Number(endTime[1])
-    //   if (currentHour == startHour && currentMinute >= startMinute) {
-    //     commit("setSignIsScheduled", true)
-    //   } else if (currentHour == endHour && currentMinute <= endMinute) {
-    //     commit("setSignIsScheduled", true)
-    //   }
-    //   else if (currentHour > startHour && currentMinute > startMinute && currentHour < endHour) {
-    //     commit("setSignIsScheduled", true)
-    //   }
-    //   else {
-    //     commit("setSignIsScheduled", false)
-    //   }
-    // },
-    // setDay({ commit, dispatch }) {
-    //   let day = ""
-    //   switch (new Date().getDay()) {
-    //     case 0:
-    //       day = "Sunday";
-    //       break;
-    //     case 1:
-    //       day = "Monday";
-    //       break;
-    //     case 2:
-    //       day = "Tuesday";
-    //       break;
-    //     case 3:
-    //       day = "Wednesday";
-    //       break;
-    //     case 4:
-    //       day = "Thursday";
-    //       break;
-    //     case 5:
-    //       day = "Friday";
-    //       break;
-    //     case 6:
-    //       day = "Saturday";
-    //   }
-    //   commit('setDay', day)
-    // }
-    ////#endregion
+
   },
   getters: {
     currentMenuItems: state => {
