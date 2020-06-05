26be166168a3ddd7a2a7e8dd5b4dfdcfc022e550
dfahn03 <dallin.code@gmail.com>
2020-06-05 16:45:25 -0600

    login page bg height fix

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,36 +6,36 @@
 </template>
 
 <script>
-import Login from "@/views/Login.vue";
-import { mapGetters } from "vuex";
-import { mapState } from "vuex";
+  import Login from "@/views/Login.vue";
+  import { mapGetters } from "vuex";
+  import { mapState } from "vuex";
 
-export default {
-  name: "APP",
-  components: {
-    Login,
-  },
-  created() {
-    this.$store.dispatch("loadLastKitchen");
-    this.$store.dispatch("loadLastSite");
-    // this.$store.dispatch("getAllSigns");
-    // this.$store.dispatch("setDay")
-  },
-  computed: {
-    ...mapGetters(["setDay"]),
-  },
-};
+  export default {
+    name: "APP",
+    components: {
+      Login,
+    },
+    created() {
+      this.$store.dispatch("loadLastKitchen");
+      this.$store.dispatch("loadLastSite");
+      // this.$store.dispatch("getAllSigns");
+      // this.$store.dispatch("setDay")
+    },
+    computed: {
+      ...mapGetters(["setDay"]),
+    },
+  };
 </script>
 
 <style scoped>
-#app {
-  font-family: "Open Sans", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: whitesmoke;
-  user-select: none;
-  padding-top: 3vh;
-  background-color: rgb(5, 38, 45);
-}
+  #app {
+    font-family: "Open Sans", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: whitesmoke;
+    user-select: none;
+    height: 100vh;
+    background-color: rgb(5, 38, 45);
+  }
 </style>
